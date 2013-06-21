59ac42fbde05dda70a4dba2fa92a184d24365c8b
Edwin Shin <edwin.shin@yourmediashelf.com>
2013-06-21 21:44:15 +0800

    Uses fcrepo4 as parent pom.
    Adds @InjectedSession

--- src/main/java/org/fcrepo/fixity/resources/FixityCorrupter.java
+++ src/main/java/org/fcrepo/fixity/resources/FixityCorrupter.java
@@ -1,3 +1,4 @@
+
 package org.fcrepo.fixity.resources;
 
 import java.io.ByteArrayInputStream;
@@ -8,6 +9,7 @@ import java.util.Set;
 
 import javax.inject.Inject;
 import javax.jcr.RepositoryException;
+import javax.jcr.Session;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -16,6 +18,7 @@ import javax.ws.rs.core.Response;
 import org.fcrepo.AbstractResource;
 import org.fcrepo.services.LowLevelStorageService;
 import org.fcrepo.services.ObjectService;
+import org.fcrepo.session.InjectedSession;
 import org.fcrepo.utils.LowLevelCacheEntry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,9 +26,13 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Path("/fixity-corrupter")
-public class FixityCorrupter extends AbstractResource{
+public class FixityCorrupter extends AbstractResource {
 
-	private static final Logger logger = LoggerFactory.getLogger(FixityCorrupter.class);
+    private static final Logger logger = LoggerFactory
+            .getLogger(FixityCorrupter.class);
+
+    @InjectedSession
+    protected Session session;
 
     @Inject
     private ObjectService objects;
@@ -33,27 +40,32 @@ public class FixityCorrupter extends AbstractResource{
     @Inject
     private LowLevelStorageService llstorage;
 
-	@POST
-	@Path("/{path}")
-	public Response corruptDatastream(@PathParam("path") final String path) throws RepositoryException, IOException {
-	    int numCorrupt = 1;
+    @POST
+    @Path("/{path}")
+    public Response corruptDatastream(@PathParam("path")
+    final String path) throws RepositoryException, IOException {
+        int numCorrupt = 1;
 
-	    final Set<LowLevelCacheEntry> cacheEntries = this.llstorage.getLowLevelCacheEntries(objects.getObjectNode(getAuthenticatedSession(),path));
+        final Set<LowLevelCacheEntry> cacheEntries =
+                this.llstorage.getLowLevelCacheEntries(objects.getObjectNode(
+                        session, path));
 
-	    /* iterate over all the lowlevel cacheentries until numCorrupt reaches 0 */
-	    for (Iterator<LowLevelCacheEntry> entryIterator = cacheEntries.iterator();entryIterator.hasNext();){
-	        if (numCorrupt-- <= 0) {
-	            break;
-	        }
-	        LowLevelCacheEntry entry = entryIterator.next();
+        /* iterate over all the lowlevel cacheentries until numCorrupt reaches 0 */
+        for (Iterator<LowLevelCacheEntry> entryIterator =
+                cacheEntries.iterator(); entryIterator.hasNext();) {
+            if (numCorrupt-- <= 0) {
+                break;
+            }
+            LowLevelCacheEntry entry = entryIterator.next();
             byte[] corrupted = new byte[4096];
             new Random().nextBytes(corrupted);
-            logger.debug("corrupting " + path + " instance: " + entry.getExternalId());
+            logger.debug("corrupting " + path + " instance: " +
+                    entry.getExternalId());
             ByteArrayInputStream src = new ByteArrayInputStream(corrupted);
             entry.storeValue(src);
-	    }
+        }
 
-		return Response.ok().build();
-	}
+        return Response.ok().build();
+    }
 
 }
